5088501ad3b836957eb08de18cc26325b15c3bfc
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-04-01 12:52:45 +0530

    custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,19 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect} from "react";
 
-function App() {
-  const [counter,setCounter]= useState(0)
-  const [inpu, setInput] = useState(1);
-  
-
-let count=useMemo(()=>{
-  console.log("memo");
-  let finalcount=0;
-  for(let i=1;i<=inpu;i++){
-    finalcount=finalcount+i;
-  }
- return finalcount;
-  
-},[inpu])
- 
-  // useEffect(() => {
-  //   setSum((inpu * (inpu + 1)) / 2);
-  // }, [inpu]);
-  
-  return (
-    <div>
-      <input
-        type="number"
-        onChange={(e) => {
-          setInput(parseInt(e.target.value) || 1);
-        }}
-        placeholder="Find sum from 1 to n"
-      />
-      <br />
-      Sum from 1 to {inpu} is {count};
-      <br />
-      <button
-      onClick={()=>{
-        setCounter(counter+1); 
-      }
-      }>counter {counter}</button>
-    </div>
-  );
+function useTodos() {
+  const [todos,setTodos]= useState([]);
+  useEffect(()=>{
+    axios.get("").then((res)=>{
+      setTodos(res.data.todos)
+    })
+  },[])
+  return todos;
 }
-
-export default App;
- 
+  
+function App(){
+  const todos=useTodos();
+  return <div>
+    {todos} 
+  </div>
+}
+export default App 
