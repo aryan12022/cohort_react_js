331b9df0b32556b19d48a0e055794d821cd88f78
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-04-02 14:31:04 +0530

    useref

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,27 @@
 import { useState, useMemo, useCallback,
-   useEffect} from "react";
+   useEffect,
+   useRef} from "react";
 
-function useSomething(){
 
-}
 
 function App(){
-  const [exchange1Data, setexchange1Data]=useState({})
-  const [exchange2Data,setexchange2Data]=useState({})
-  const [bankData,setBankData]=useState({})
+ const [incomeTax,setIncomeTax]=useState(2000)
   
+const divref =useRef();
+
 useEffect(()=>{
-  setexchange1Data({
-    returns:100
-  });
+  setTimeout(() => {
+    console.log(divref.current);
+    divref.current.innerHTML=10
+  }, 5000);
+ 
 },[])
-useEffect(()=>{
-  setexchange2Data({
-    returns:100
-  })
-},[])
-useEffect(()=>{  
-setTimeout(() => {
-  setBankData({
-    income:100
-  });
-},5000)
-},[]);
 
-const calculateCryptoReturns = function(){
-  return exchange1Data.returns+exchange2Data.returns;
-}
-
-const incomeTax=(calculateCryptoReturns()+bankData.income)*0.3;
-  return (
-    <div>
-    <Child calculateCryptoReturns ={calculateCryptoReturns}/>
-    </div>
-  )
-}
-
-function Child({calculateCryptoReturns}){
-  console.log("crypto child re-rendered");
-  return <div>
-    your crypto returns are {calculateCryptoReturns()}
+return (
+  <div>
+  hi there , your income tax returns are <div ref={divref}>{incomeTax}</div>
   </div>
+)
   
 }
 export default App 
