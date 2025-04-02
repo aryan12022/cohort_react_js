c2c410f78e1e746f91f98abda9ce5ee6cc95ea00
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-04-02 14:21:55 +0530

    react recap till now

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
-import { useState, useEffect} from "react";
+import { useState, useMemo, useCallback,
+   useEffect} from "react";
 
-function useTodos() {
-  const [todos,setTodos]= useState([]);
-  useEffect(()=>{
-    axios.get("").then((res)=>{
-      setTodos(res.data.todos)
-    })
-  },[])
-  return todos;
+function useSomething(){
+
 }
-  
+
 function App(){
-  const todos=useTodos();
+  const [exchange1Data, setexchange1Data]=useState({})
+  const [exchange2Data,setexchange2Data]=useState({})
+  const [bankData,setBankData]=useState({})
+  
+useEffect(()=>{
+  setexchange1Data({
+    returns:100
+  });
+},[])
+useEffect(()=>{
+  setexchange2Data({
+    returns:100
+  })
+},[])
+useEffect(()=>{  
+setTimeout(() => {
+  setBankData({
+    income:100
+  });
+},5000)
+},[]);
+
+const calculateCryptoReturns = function(){
+  return exchange1Data.returns+exchange2Data.returns;
+}
+
+const incomeTax=(calculateCryptoReturns()+bankData.income)*0.3;
+  return (
+    <div>
+    <Child calculateCryptoReturns ={calculateCryptoReturns}/>
+    </div>
+  )
+}
+
+function Child({calculateCryptoReturns}){
+  console.log("crypto child re-rendered");
   return <div>
-    {todos} 
+    your crypto returns are {calculateCryptoReturns()}
   </div>
+  
 }
 export default App 
